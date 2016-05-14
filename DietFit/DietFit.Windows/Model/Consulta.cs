57a2bc5a180c5bf570ad5989c4a0fe3e4e884617
mariobt <a027958@ismai.pt>
2016-05-14 18:18:04 +0100

    Configuraçao da view criar consulta nutriçao e respetivas classes

---
+++ DietFit/DietFit.Windows/Model/Consulta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietFit.Model
+{
+    public class Consulta
+    {
+        Utilizador nutricionista;
+        Utilizador cliente;
+        DateTime data;
+
+
+        public Consulta(Utilizador nutricionista, Utilizador cliente, DateTime data)
+        {
+            this.nutricionista = nutricionista;
+            this.cliente = cliente;
+            this.data = data;
+        }
+    }
+}
---
+++ DietFit/DietFit.Windows/Model/RepositorioConsulta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DietFit.Model
+{
+    public class RepositorioConsulta
+    {
+        List<Consulta> consultas ;
+
+
+        public RepositorioConsulta()
+        {
+            consultas = new List<Consulta>();
+        }
+
+        public void addConsulta(Consulta c)
+        {
+            consultas.Add(c);
+        }
+        public void removeConsulta(Consulta c)
+        {
+            consultas.Remove(c);
+        }
+        public bool containConsulta(Consulta c)
+        {
+            if (consultas.Contains(c))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void findbyUser(List<Consulta> User)
+        {
+
+        }
+
+    
+    }
+}
