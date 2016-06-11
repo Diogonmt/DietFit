24e7124c27291256b0d00ca0a423050cec9a0378
mlfazevedo <marcos.luis.f.a@hotmail.com>
2016-06-11 16:56:32 +0100

    Criaçao classe contacto nutricionista

---
+++ DietFit/DietFit.Windows/Controllers/ContactoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DietFit.Model;
+
+namespace DietFit.Controllers
+{
+    class ContactoController
+    {
+        Appl app;
+        Utilizador nutricionista;
+
+
+        public ContactoController(Appl app, Utilizador nutricionista)
+        {
+            this.nutricionista = nutricionista;
+            this.app = app;
+        }
+
+        public List<Utilizador> getUtilizadores()
+        {
+            return app.getUtilizadores();
+        }
+
+        public Utilizador getUtilizadorbyUsername(String username)
+        {
+            return app.getUtilizadorByUser(username);
+        }
+    }
+}
---
+++ DietFit/DietFit.Windows/Model/ContactoNutricionista.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DietFit.Model;
+
+namespace DietFit.Model
+{
+    class ContactoNutricionista
+    {
+        Utilizador nutricionista;
+        Utilizador cliente;
+    }
+}
